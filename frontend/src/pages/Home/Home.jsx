57b8a16709e4239a87b68bc67b308ae9741e8c59
Sunphetcharaj <153702282+Sunphetcharaj@users.noreply.github.com>
2024-08-09 02:44:43 +0000

    Fixed small issues and applied styling

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { MdAdd } from "react-icons/md";
 import Modal from "react-modal";
 import AddEditNotes from "./AddEditNotes";
 import Toast from "../../components/ToastMessage/Toast";
-import axiosinstance from "../../utils/axiosinstance";
+import axiosinstance from "../../../utils/axiosinstance";
 import { useNavigate } from "react-router-dom";
 import AddNotesImg from "../../assets/images/add-notes.svg";
 import NoDataImg from "../../assets/images/no-data.svg";
